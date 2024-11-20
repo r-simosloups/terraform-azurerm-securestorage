7b0f2b06e2efa3a6bd0ab97dd0184c3b945704c7
RS <RS>
2024-11-20 14:47:50 +0100

    adding conf terraform

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "resource_group_name" {
+  type = string
+  description = "resource group name"
+}
+
+variable "location" {
+  type = string
+  description = "set the location"
+}
+
+variable "storage_account_name" {
+  type = string
+  description = "set the storage account name"
+}
+
+variable "environment" {
+  type = string
+  default = "set the environment type"
+}
+
